95d9ae72ff6c075028749feba6a8c0ca066a41d1
Galis <galis.travelling@gmail.com>
2024-04-22 11:16:13 +0200

    header

---
+++ src/components/MainHeader.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import logoImg from "@/assets/logo.png";
+
+export const MainHeader = () => {
+  return (
+    <header className="flex justify-center">
+      <Link href="/">
+        <Image src={logoImg} alt="Logo" className="size-36 md:size-48" />
+      </Link>
+    </header>
+  );
+};
